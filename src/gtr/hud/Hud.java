4e76407cddebb3a480ef5e8ad236c4c64afbd0f0
Maximilian Stauss <max.stauss@gmail.com>
2013-03-18 12:15:10 +0100

    added TestAmmo

--- src/gtr/hud/Hud.java
+++ src/gtr/hud/Hud.java
@@ -7,8 +7,12 @@ public class Hud {
 	private static String hud = createHud();
 	
 	private static String createHud() {
-		String statusBar = " KP: "; // die ersten fünf Zeichen
+		String statusBar = " LP: "; // die ersten fünf Zeichen
 		statusBar += Integer.toString(Messenger.getPlayer().getHp());
+		
+		statusBar += "	";
+		
+		statusBar += " Ammo: ∞";
 		
 		
 		return statusBar;
