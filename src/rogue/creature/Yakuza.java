cb7ff70f463e15a23607561de16ca32af9356845
Mitamo <mitamo@zedat.fu-berlin.de>
2013-03-20 15:03:25 +0100

    Yakuza Bewebung vollständig, es fehlt text

--- src/rogue/creature/Yakuza.java
+++ src/rogue/creature/Yakuza.java
@@ -1,7 +1,9 @@
 package rogue.creature;
 
 import java.awt.Color;
+import java.util.Arrays;
 
+import jade.util.Dice;
 import jade.util.datatype.ColoredChar;
 import jade.util.datatype.Direction;
 
@@ -11,7 +13,7 @@ public class Yakuza extends Monster
 	
 		public Yakuza() 
 		{
-		super(ColoredChar.create('Y', new Color(0,200,0)));
+		super(ColoredChar.create('Y', new Color(139,026,026)));
 		// TODO Auto-generated constructor stub
 		}
 		
@@ -32,16 +34,15 @@ public class Yakuza extends Monster
 			 switch (getQuadrant())
 			 {
 			 case 1:if((y)<=r-2){move(0,2); angriff();}else if((y)==r-1){move(0,1); angriff();}else if((y)==r){move(0,0); angriff();}else if((y)>r){move(0,-2);}break;
-			 case 2:break;
+			 case 2:if(x>=y){if(y>=2){move (0,-2);angriff();} else {move(0,-1);angriff();}} else {if(x>=2){move (-2,0);angriff();} else {move(-1,0);angriff();}} break;
 			 case 3:if((x)<=r-2){move(2,0); angriff();}else if((x)==r-1){move(1,0); angriff();}else if((x)==r){move(0,0); angriff();}else if((x)>r){move(-2,0);}break;
-			 case 4:break;
+			 case 4:if(x>=y){if(y>=2){move (0,2);angriff();} else {move(0,1);angriff();}} else {if(x>=2){move (-2,0);angriff();} else {move(-1,0);angriff();}} break;
 			 case 5:if((y)<=r-2){move(0,-2); angriff();}else if((y)==r-1){move(0,-1); angriff();}else if((y)==r){move(0,0); angriff();}else if((y)>r){move(0,2);}break;
-			 case 6:break;
+			 case 6:if(x>=y){if(y>=2){move (0,2);angriff();} else {move(0,1);angriff();}} else {if(x>=2){move (2,0);angriff();} else {move(-1,0);angriff();}} break;
 			 case 7:if((x)<=r-2){move(-2,0); angriff();}else if((x)==r-1){move(-1,0); angriff();}else if((x)==r){move(0,0); angriff();}else if((x)>r){move(3,0);}break;
-			 case 8:break;
-			 case 9:break;
+			 case 8:if(x>=y){if(y>=2){move (0,-2);angriff();} else {move(0,-1);angriff();}} else {if(x>=2){move (2,0);angriff();} else {move(1,0);angriff();}} break;
+			 case 9:move(Dice.global.choose(Arrays.asList(Direction.values())));angriff();break;
 			 }
-			 angriff();
 		 	} 		 	
 	 else									//Spieler ausserhalb der Reichweite
 	{
