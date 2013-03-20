55393723fb333c741ab9d2a90e035aa73ba84bd2
Mitamo <mitamo@zedat.fu-berlin.de>
2013-03-20 04:45:37 +0100

    Kleinkram

---
+++ src/rogue/creature/Yakuza.java
@@ -0,0 +1,25 @@
+package rogue.creature;
+
+import java.awt.Color;
+
+import jade.util.datatype.ColoredChar;
+
+public class Yakuza extends Monster
+{
+
+	
+		public Yakuza() 
+		{
+		super(ColoredChar.create('Y', new Color(0,200,0)));
+		// TODO Auto-generated constructor stub
+		}
+		
+		public void act() 
+		{
+		
+		//Direction dir = findPlayerInRange();
+		//if(dir != null)
+		//	{ //angreifbar
+		//	  attack(dir, weapon);}
+		}
+}
